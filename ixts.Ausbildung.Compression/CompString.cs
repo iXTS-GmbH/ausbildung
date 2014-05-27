a4aab66e0e90bb0331a75a21d3bdddef75b4aaf0
unknown <mkaestl@XDEV007.ixts.local>
2014-05-27 12:16:00 +0200

    Encode Methode geschrieben

--- ixts.Ausbildung.Compression/CompString.cs
+++ ixts.Ausbildung.Compression/CompString.cs
@@ -3,5 +3,14 @@ using System.Text;
 
 namespace ixts.Ausbildung.Compression
 {
-    
+    public class RunLengthEncoder
+    {
+        public String Encode(String str)
+        {
+            var sBuilder = new StringBuilder();
+            sBuilder.Append("");
+            return sBuilder.ToString();
+        }
+
+    }
 }
