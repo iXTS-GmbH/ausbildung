6482934f201d66b52dc8c6416f60aabd61556b5e
unknown <mkaestl@XDEV007.ixts.local>
2014-05-27 12:20:31 +0200

    GetNextGroup geschrieben

--- ixts.Ausbildung.Compression/CompString.cs
+++ ixts.Ausbildung.Compression/CompString.cs
@@ -5,12 +5,43 @@ namespace ixts.Ausbildung.Compression
 {
     public class RunLengthEncoder
     {
+        private char lastElement;
+        private int point;
+
         public String Encode(String str)
         {
             var sBuilder = new StringBuilder();
-            sBuilder.Append("");
+            sBuilder.Append(GetNextGroup(str));
             return sBuilder.ToString();
         }
+
+        public String GetNextGroup(String str)
+        {
+            var group = new StringBuilder();
+            for (var i = point; i < str.Length; i++)
+            {
+                if (str[i] == lastElement || i == 0)
+                {
+                    if (group.Length == 9)
+                    {
+                        point = i;
+                        return group.ToString();
+                    }
+                    lastElement = str[i];
+                    group.Append(str[i]);
+                }
+                else
+                {
+                    lastElement = str[i];
+                    point = i;
+                    return group.ToString();
+
+                }
+            }
+            point = str.Length;
+            return group.ToString();
+        }
+
 
     }
 }
