eb1804a341ca7ab556b90bb2ef0df85414266dc9
unknown <mkaestl@XDEV007.ixts.local>
2014-07-07 11:18:01 +0200

    Tests für Distance-Methode geschrieben

--- ixts.Ausbildung.Geometry.Test/PointTests.cs
+++ ixts.Ausbildung.Geometry.Test/PointTests.cs
@@ -9,15 +9,36 @@ namespace ixts.Ausbildung.Geometry.Test
     [TestFixture]
     public class PointTests
     {
-        private Point sut;
-
-        [TestCase(1.0,2.0,1.0,2.0)]
+        [TestCase(1,2,1,2)]
         public void KonstruktorTest(double expectedX,double expectedY, double x, double y)
         {
 
             var actual = new Point(x, y);
-            Assert.AreEqual(expectedX, actual.x);
-            Assert.AreEqual(expectedY, actual.y);
+            Assert.AreEqual(expectedX, actual.X());
+            Assert.AreEqual(expectedY, actual.Y());
+        }
+        [TestCase(4,4)]
+        public void XTest(double expected, double x)
+        {
+            var actual = new Point(x, 0.0);
+            Assert.AreEqual(expected, actual.X());
+        }
+        [TestCase(4,4)]
+        public void YTest(double expected, double y)
+        {
+            var actual = new Point(0.0, y);
+            Assert.AreEqual(expected, actual.Y());
+        }
+
+        [TestCase(0,1,0,2,1)]//  X-Gleich
+        [TestCase(1,0,1,0,1)]// Y-Gleich
+        [TestCase(1,2,2,1,1)]// Beide Verschieden
+        public void DistanceTest(double PointX, double PointY, double NPointX, double NPointY, double expected)
+        {
+            var Point = new Point(PointX, PointY);
+            var NPoint = new Point(NPointX, NPointY);
+            var actual = Point.Distance(NPoint);
+            Assert.AreEqual(expected,actual);
         }
     }
 }
--- ixts.Ausbildung.Geometry/Point.cs
+++ ixts.Ausbildung.Geometry/Point.cs
@@ -7,12 +7,29 @@ namespace ixts.Ausbildung.Geometry
 {
     public class Point
     {
-        public double x;
-        public double y;
+        protected double x;
+        protected double y;
         public Point(double x,double y)
         {
             this.x = x;
             this.y = y;
         }
+
+        public double X()
+        {
+            return x;
+        }
+
+        public double Y()
+        {
+            return y;
+        }
+
+        public double Distance(object NPoint)
+        {
+
+            return 0;
+        }
+
     }
 }
