5893b76f43e66e7eea48ecc4428b374c62924a22
unknown <mkaestl@XDEV007.ixts.local>
2014-07-11 08:53:48 +0200

    Parallelogram Methode geschrieben

--- ixts.Ausbildung.Geometry/Quadrilateral.cs
+++ ixts.Ausbildung.Geometry/Quadrilateral.cs
@@ -21,7 +21,9 @@ namespace ixts.Ausbildung.Geometry
 
         public Boolean Parallelogram()
         {
-            return false;
+            var check = Math.Round(Points[0].Distance(Points[1]),3) == Math.Round(Points[2].Distance(Points[3]),3) &&
+                        Math.Round(Points[1].Distance(Points[2]),3) == Math.Round(Points[3].Distance(Points[0]),3);
+            return check;
         }
     }
 }
