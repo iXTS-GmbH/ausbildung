c39494f12cbeb2ba85a9d15b5c1d97cc75a2b09b
unknown <mkaestl@XDEV007.ixts.local>
2014-07-14 08:53:10 +0200

    Eine Methode für jeden Befehl erstellt

--- ixts.Ausbildung.Geometry.PrinterConsole/Program.cs
+++ ixts.Ausbildung.Geometry.PrinterConsole/Program.cs
@@ -9,8 +9,42 @@ namespace ixts.Ausbildung.Geometry.PrinterConsole
     {
         static void Main(string[] args)
         {
-
+            switch (args[0])
+            {
+                case "draw":
+                    Draw(args);
+                    break;
+                case "move":
+                    Move(args);
+                    break;
+                case "zoom":
+                    Zoom(args);
+                    break;
+                case "print":
+                    Print(args);
+                    break;
+            }
             
+        }
+
+        private static void Draw(string[] args)
+        {
+            //Methode Draw
+        }
+
+        private static void Move(string[] args)
+        {
+            //Methode Move
+        }
+
+        private static void Zoom(string[] args)
+        {
+            //Methode Zoom
+        }
+
+        private static void Print(string[] args)
+        {
+            //Methode Print
         }
     }
 }
