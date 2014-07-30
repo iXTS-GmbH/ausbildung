ace7b193e6a44b5daae14723b6823c33a21345b1
unknown <mkaestl@XDEV007.ixts.local>
2014-07-30 09:09:44 +0200

    Aufgabe Römische Zahlen leeres Projekt

---
+++ ixts.Ausbildung.Roman.Test/RomanTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ixts.Ausbildung.Roman.Test
+{
+    [TestFixture]
+    public class RomanTests
+    {
+    }
+}
---
+++ ixts.Ausbildung.Roman/Roman.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ixts.Ausbildung.Roman
+{
+    public class Roman
+    {
+    }
+}
