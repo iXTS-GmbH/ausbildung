6535747831a6f62e5b66c576bec1d35392dc9d08
unknown <mkaestl@XDEV007.ixts.local>
2014-08-05 09:47:34 +0200

    Ende Überarbeitung, Ende Roman

--- ixts.Ausbildung.Roman/Roman.cs
+++ ixts.Ausbildung.Roman/Roman.cs
@@ -9,21 +9,23 @@ namespace ixts.Ausbildung.Roman
         private static readonly Dictionary<String, int> RomanToNumericMap = new Dictionary<String, int>
             {
                 { "M", 1000 },
-                { "CM", 900 }, 
-                { "D",  500 }, 
-                { "CD", 400 }, 
-                { "C",  100 }, 
-                { "XC",  90 }, 
-                { "L",   50 }, 
-                { "XL",  40 }, 
-                { "X",   10 }, 
-                { "IX",   9 }, 
-                { "V",    5 }, 
-                { "IV",   4 }, 
+                { "CM", 900 },
+                { "D",  500 },
+                { "CD", 400 },
+                { "C",  100 },
+                { "XC",  90 },
+                { "L",   50 },
+                { "XL",  40 },
+                { "X",   10 },
+                { "IX",   9 },
+                { "V",    5 },
+                { "IV",   4 },
                 { "I",    1 }
             };
         private readonly int romanNumber;
-        private const int MAX_EQUAL_CHARAKTER_I_A_ROW = 3;
+        private const int MAX_EQUAL_CHARACTER_IN_A_ROW = 3;
+        private const int MIN_ROMAN_NUMBER_VALUE = 1;
+        private const int MAX_ROMAN_NUMBER_VALUE = 3999;
         public static IComparer<Roman> LengthComparator = new LengthComparator();
         public static IComparer<Roman> LexicalComparator = new LexicalComparator();
 
@@ -39,7 +41,7 @@ namespace ixts.Ausbildung.Roman
             }
             if (!ValidateRomanNumber(ToNumeral(literal)))
             {
-                throw new ArgumentException("Zahl besitzt ungültigen Wert, Wert darf nicht größer als 3999 sein");
+                throw new ArgumentException(string.Format("Zahl besitzt ungültigen Wert, Wert muss zwischen {0} und {1} liegen",MIN_ROMAN_NUMBER_VALUE,MAX_ROMAN_NUMBER_VALUE));
             }
             romanNumber = ToNumeral(literal);
         }
@@ -48,22 +50,31 @@ namespace ixts.Ausbildung.Roman
         {
             if (!ValidateRomanNumber(number))
             {
-                throw new ArgumentException(string.Format("{0} ist kein gültiger Wert. Wert muss zwischen 1 und 3999 liegen", number));   
+                throw new ArgumentException(string.Format("{0} ist kein gültiger Wert. Wert muss zwischen {1} und {2} liegen", number,MIN_ROMAN_NUMBER_VALUE,MAX_ROMAN_NUMBER_VALUE));   
             }
+
             romanNumber = number;
         }
 
-        private Boolean ValidateRomanNumber(String literal)
+        private static Boolean ValidateRomanNumber(String literal)
         {
             var equalCharacterInARow = 1;
-            var marker = literal[0];
+            var lastCharacter = literal[0];
+
             for (var i = 1; i < literal.Length; i++)
             {
-                if (marker == literal[i])
+                var character = literal[i];
+
+                if (!RomanToNumericMap.ContainsKey(character.ToString()))
+                {
+                    return false;
+                }
+
+                if (lastCharacter == character)
                 {
                     equalCharacterInARow += 1;
 
-                    if (equalCharacterInARow > MAX_EQUAL_CHARAKTER_I_A_ROW)
+                    if (equalCharacterInARow > MAX_EQUAL_CHARACTER_IN_A_ROW)
                     {
                         return false;
                     }
@@ -73,51 +84,46 @@ namespace ixts.Ausbildung.Roman
                     equalCharacterInARow = 1;
                 }
 
-                marker = literal[i];
+                lastCharacter = character;
             }
+
             return true;
         }
 
-        private Boolean ValidateRomanNumber(int number)
+        private static Boolean ValidateRomanNumber(int number)
         {
-            return number < 4000 && number > 0;
+            return number <= MAX_ROMAN_NUMBER_VALUE && number >= MIN_ROMAN_NUMBER_VALUE;
         }
 
         public int ToNumeral()
         {
-            return ToNumeral(ToLiteral(romanNumber));
+            return romanNumber;
         }
 
-        public int ToNumeral(String literal)
+        private static int ToNumeral(String literal)
         {
             var number = 0;
             var lastvalue = 0;
+
             for ( var i = 0;i < literal.Length;i++)
             {
-                var letterValue = ParseCharToNumber(literal[i]);
-                if (i == 0 || lastvalue >= letterValue)
-                {//Addieren
-                    number += letterValue;
+                var characterValue = RomanToNumericMap[Convert.ToString(literal[i])];
+
+                if (i == 0 || lastvalue >= characterValue)
+                {
+                    number += characterValue;
                 }
                 else
-                {//Subtrahieren
+                {
                     number -= lastvalue;
-                    number += (letterValue - lastvalue);
+                    number += (characterValue - lastvalue);
                     
                 }
-                lastvalue = letterValue;
+
+                lastvalue = characterValue;
             }
 
             return number;
-        }
-
-        private static int ParseCharToNumber(char letter)
-        {
-            if (RomanToNumericMap.ContainsKey(Convert.ToString(letter)))
-            {
-                return RomanToNumericMap[Convert.ToString(letter)];
-            }
-            throw new ArgumentException(string.Format("{0} ist kein gültiges Zeichen für eine Römische Zahl", letter));
         }
 
         private static String ToLiteral(int number)
@@ -137,7 +143,7 @@ namespace ixts.Ausbildung.Roman
             return literal.ToString();
         }
 
-        public static Roman operator+(Roman roman, Roman otherRoman)
+        public static Roman operator +(Roman roman, Roman otherRoman)
         {
             var sum = roman.ToNumeral() + otherRoman.ToNumeral();
 
@@ -146,40 +152,44 @@ namespace ixts.Ausbildung.Roman
 
         public static Roman operator -(Roman roman, Roman otherRoman)
         {
-            var dif = roman.ToNumeral() - otherRoman.ToNumeral();
+            var diff = roman.ToNumeral() - otherRoman.ToNumeral();
 
-            return new Roman(dif);
+            return new Roman(diff);
         }
 
         public static Roman operator *(Roman roman, Roman otherRoman)
         {
-            var multi = roman.ToNumeral() * otherRoman.ToNumeral();
+            var product = roman.ToNumeral() * otherRoman.ToNumeral();
 
-            return new Roman(multi);
+            return new Roman(product);
         }
 
         public static Roman operator /(Roman roman, Roman otherRoman)
         {
             var divide = roman.ToNumeral() / (double)otherRoman.ToNumeral();
+
             if (ValidateDivide(divide))
             {
                 return new Roman((int)divide);
             }
+
             throw new ArgumentException("Der Quotient der zu Dividierenden Zahlen muss Ganzzahlig sein ");
         }
 
         private static Boolean ValidateDivide(double divide)
         {
-            return !(divide - (int) divide > 0);
+            return divide - (int) divide <= 0;
         }
 
         public override Boolean Equals(object other)
         {
             var otherRoman = other as Roman;
+
             if (otherRoman == null)
             {
                 return false;
             }
+
             return GetHashCode() == otherRoman.GetHashCode();
         }
 
@@ -192,7 +202,5 @@ namespace ixts.Ausbildung.Roman
         {
             return ToLiteral(romanNumber);
         }
-
-
     }
 }
