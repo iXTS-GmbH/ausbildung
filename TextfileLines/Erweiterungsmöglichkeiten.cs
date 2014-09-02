d2554241cebf609e99a9fcacf9854bc41668245f
unknown <mkaestl@XDEV007.ixts.local>
2014-09-02 15:50:54 +0200

    Erweiterungsmöglichkeiten hinzugefügt

---
+++ TextfileLines/Erweiterungsmöglichkeiten.cs
@@ -0,0 +1,26 @@
+﻿namespace TextFileLines
+{
+    class Erweiterungsmöglichkeiten
+    {
+        //TODO Erweiterungen
+        //
+        //Erweiterungsmöglichkeiten:
+        //
+        //Klasse einfügen die Wortweises Iterieren durch Textdokumente erlaubt
+            //Nur Mitgegebenes Wort in Uppercase schreiben
+            //Entfernt alle exemplare eines Mitgegebenen Wortes aus dem Text
+            //Ab bestimmter Anzahl Wörter spalten
+            //Klasse einfügen die bestimmtes Wort sucht
+        //Klasse einfügen die Zeichenweises Iterieren durch Textdokumente erlaubt
+            //Alle exemplare eines bestimmten Buchstaben Uppercase schreiben
+            //alle ä,ö,ü durch ae,oe,ue ersetzten
+            //Alles in 1337-Sprache ändern
+        //Klasse einfügen die Zeilen/Wörter/Zeichen zählen kann
+        //Klasse einfügen die Zeilen mit bestimmtem Anfang löscht
+        //Klasse einfügen die alles in Lowercase schreibt
+        //Klasse einfügen die alle Textdokumente aus einem Verzeichnis nimmt und in einem Textdokument(Complete.txt) zusammenschreibt
+        //Klasse einfügen die nach jeder x'ten zeile spaltet 
+        //Klasse einfügen die jede x'te zeile in Uppercase/Lowercase/1337 schreibt
+        //
+    }
+}
