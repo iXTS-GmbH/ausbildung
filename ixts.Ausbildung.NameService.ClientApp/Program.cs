0c64db9b7c25d155cbb0c7c7d4f869665031f314
unknown <mkaestl@XDEV007.ixts.local>
2014-09-08 14:23:39 +0200

    Client um ReadLinesequenz erweitert

--- ixts.Ausbildung.NameService.ClientApp/Program.cs
+++ ixts.Ausbildung.NameService.ClientApp/Program.cs
@@ -9,14 +9,21 @@ namespace ixts.Ausbildung.NameService.ClientApp
             String host = args.Length > 0 ? args[0] : "localhost";
             int port = args.Length > 1 ? Int32.Parse(args[1]) : 2000;
             NameClient nc = new NameClient(host, port);
-            Console.WriteLine(nc.Action("PUT", "Inge", "localhost"));
-            Console.WriteLine(nc.Action("PUT", "Inge", "127.0.0.1"));
-            Console.WriteLine(nc.Action("GET", "Inge", "null"));
-            Console.WriteLine(nc.Action("DEL", "Inge", "null"));
-            Console.WriteLine(nc.Action("GET", "Inge", "null"));
-            //Hierhin kommen die Aktionen im Format:
-            //Console.WriteLine(nc.Aktion("BEFEHL","Key","Value||null"));
+            String line;
+            String value;
+            Boolean run = true;
 
+            while(run)
+            {
+                line = Console.ReadLine();
+                String[] parameters = line.Split(' ');
+                value = parameters.Length > 2 ? parameters[2]: null
+                Console.WriteLine(nc.Action(parameters[0],parameters[1],value))
+                if(parameters[0] == "STOP")
+                {
+                    run = false;
+                }
+            }
         }
     }
 }
