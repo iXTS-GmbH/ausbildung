7c32f3d1612ba6f1e470e79b5fd0405be194c609
unknown <mkaestl@XDEV007.ixts.local>
2014-10-30 12:58:28 +0100

    Test für die Goldbach'sche Vermutung geschrieben

---
+++ ixts.Ausbildung.Primzahlen.Test/GoldbachVermutungTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ixts.Ausbildung.Primzahlen.Test
+{
+    [TestFixture]
+    public class GoldbachVermutungTests
+    {
+        [TestCase(1000000)]
+        public void TestGoldbachTest(int number)
+        {
+            Assert.IsTrue(GoldbachVermutung.TestGoldbach(number));
+        }
+    }
+}
