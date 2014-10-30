54ee69e01cefacf45bf7a27da834121305a4aa3b
unknown <mkaestl@XDEV007.ixts.local>
2014-10-30 14:05:08 +0100

    ConsoleApp erweitert

--- ixts.Ausbildung.Primzahlen.ConsoleApp/Program.cs
+++ ixts.Ausbildung.Primzahlen.ConsoleApp/Program.cs
@@ -6,16 +6,36 @@ namespace ixts.Ausbildung.Primzahlen.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wollen sie alle Primzahlen bis zu einem Wert anzeigen(p) oder eine zufällige Primzahl generieren(g)?");
+            Console.WriteLine("Was wollen sie tun?");
+            Console.WriteLine();
+            Console.WriteLine("Alle Primzahlen bis zu einem Wert ermitteln lassen (AP)");
+            Console.WriteLine("Eine zufällige Primzahl generieren lassen (RP)");
+            Console.WriteLine("Prüfen ob eine Zahl eine Primzahl ist (CP)");
+            Console.WriteLine("Einen Generator zu einer Primzahl ermitteln lassen (GG)");
+            Console.WriteLine("Prüfen ob eine Zahl ein Generator für eine Primzahl ist (CG)");
             var command = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(command))
+            {
+                command = command.ToUpper();
+            }
 
             switch (command)
             {
-                case "p":
+                case "AP":
                     GetAllPrimesUpToValue();
                     break;
-                case "g":
+                case "RP":
                     GetRandomPrime();
+                    break;
+                case "CP":
+                    CheckPrime();
+                    break;
+                case "GG":
+                    GetGenerator();
+                    break;
+                case "CG":
+                    CheckGenerator();
                     break;
                 default:
                     throw new ArgumentException(string.Format("{0} ist kein gültiger Befehl",command));
@@ -26,15 +46,12 @@ namespace ixts.Ausbildung.Primzahlen.ConsoleApp
         {
             Console.WriteLine("Bitte geben sie den Grenzwert ein");
             var line = Console.ReadLine();
-            var value = 0;
             if (!String.IsNullOrEmpty(line))
-            {
-                value = int.Parse(line);
-            }
-            else
             {
                 throw new ArgumentException("Kein Grenzwert angegeben");
             }
+            var value = int.Parse(line);
+
             for (var i = 2; i < value; i++)
             {
                 if (PrimTest.IsPrime(i))
@@ -50,6 +67,58 @@ namespace ixts.Ausbildung.Primzahlen.ConsoleApp
           Console.WriteLine(Fermat.RandomPrime());
         }
 
+        private static void CheckPrime()
+        {
+            Console.WriteLine("Bitte geben sie die zu prüfende Zahl ein");
+            var line = Console.ReadLine();
+            if (String.IsNullOrEmpty(line))
+            {
+                throw new ArgumentException("Keine Zahl angegeben");
+            }
+            var value = int.Parse(line);
+
+
+            Console.WriteLine(PrimTest.IsPrime(value) ? "Ihre Zahl ist eine Primzahl" : "Ihre Zahl ist keine Primzahl");
+        }
+
+        private static void GetGenerator()
+        {
+            Console.WriteLine("Bitte geben sie eine Primzahl ein");
+            var line = Console.ReadLine();
+            if (String.IsNullOrEmpty(line) || !PrimTest.IsPrime(int.Parse(line)))
+            {
+                throw new ArgumentException("Keine Primzahl angegeben");
+            }
+
+            var value = int.Parse(line);
+
+            var generator = PrimeGenerator.GetGenerator(value);
+
+            Console.WriteLine("Ein Generator von {0} ist {1}",value,generator);
+        }
+
+        private static void CheckGenerator()
+        {
+            Console.WriteLine("Bitte geben sie eine Primzahl ein");
+            var primeLine = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(primeLine) || !PrimTest.IsPrime(int.Parse(primeLine)))
+            {
+                throw new ArgumentException("Keine Primzahl angegeben");
+            }
+            var prime = int.Parse(primeLine);
+
+            Console.WriteLine("Bitte geben sie den zu Prüfenden Generator ein");
+            var generatorLine = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(generatorLine))
+            {
+                throw new ArgumentException("Keine Zahl angegeben");
+            }
+            var generator = int.Parse(generatorLine);
+
+            Console.WriteLine(PrimeGenerator.IsGenerator(prime, generator) ? "Ihre Zahl ist ein Generator dieser Primzahl" : "Ihre Zahl ist kein Generator dieser Primzahl");
+        }
     }
 }
             
