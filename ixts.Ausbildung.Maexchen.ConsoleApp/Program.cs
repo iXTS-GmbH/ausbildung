ebf965fbe3a1ba4ae443330caded630b6d750969
unknown <mkaestl@XDEV007.ixts.local>
2014-11-03 13:28:56 +0100

    Programm Maexchen hinzugefügt

---
+++ ixts.Ausbildung.Maexchen.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ixts.Ausbildung.Maexchen.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte geben sie ihren Würfelwurf ein");
+            var response = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(response) || string.IsNullOrWhiteSpace(response))
+            {
+                throw new Exception("Keine Angabe");
+            }
+
+            var parameters = response.Split(' ');
+
+            if (parameters.Length < 2)
+            {
+                throw new Exception("Ungenuegende Angabe");
+            }
+
+            Console.WriteLine("Sie haben");
+            Console.WriteLine("{0} Punkte", PointCalculator.GetPoints(int.Parse(parameters[0]), int.Parse(parameters[1])));
+        }
+    }
+}
---
+++ ixts.Ausbildung.Maexchen/PointCalculator.cs
@@ -0,0 +1,28 @@
+﻿namespace ixts.Ausbildung.Maexchen
+{
+    public class PointCalculator
+    {
+        public static int GetPoints(int value1, int value2)
+        {
+            if (value1 == value2)
+            {
+                return 100*value1;
+            }
+
+            if (value1 > value2)
+            {
+                if (value1 == 2)
+                {
+                    return 1000;
+                }
+                return 10*value1 + value2;
+            }
+
+            if (value2 == 2)
+            {
+                return 1000;
+            }
+            return 10*value2 + value1;
+        }
+    }
+}
