1505534efb80a88efad4080f65fca165751ed266
unknown <mkaestl@XDEV007.ixts.local>
2014-11-03 12:19:47 +0100

    Programm Zaehlmass hinzugefügt

---
+++ ixts.Ausbildung.Zaehlmass.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ixts.Ausbildung.Zaehlmass.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bitte geben sie die umzuwandelnde Zahl ein");
+            var response = Console.ReadLine();
+            if (string.IsNullOrEmpty(response) || string.IsNullOrWhiteSpace(response))
+            {
+                throw new Exception("Keine Zahl angegeben");
+            }
+            Console.WriteLine("Umgewandelt");
+            Console.WriteLine(ZaehlmassUmrechner.GetOldZaehlmass(int.Parse(response)));
+        }
+    }
+}
