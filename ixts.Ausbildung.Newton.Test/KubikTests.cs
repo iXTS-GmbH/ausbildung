5397d65d3d88df0a7f3ebfeea38edfe3e0cc2e6e
unknown <mkaestl@XDEV007.ixts.local>
2014-11-04 13:36:32 +0100

    Aufgabe Newton geschrieben

---
+++ ixts.Ausbildung.Newton.Test/KubikTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace ixts.Ausbildung.Newton.Test
+{
+    [TestFixture]
+    public class KubikTests
+    {
+        [TestCase(216,6)]
+        [TestCase(27,3)]
+        public void CalculateTest(double number,double expected)
+        {
+            var actual = Kubik.Calculate(number);
+
+            Assert.AreEqual(expected, actual, 0.000000001);
+        }
+
+    }
+}
