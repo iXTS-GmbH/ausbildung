e16eaff4217ebf912b235e96dffbcb754e2fe4f3
unknown <mkaestl@XDEV007.ixts.local>
2014-11-04 13:12:11 +0100

    Aufgabe Newton Teil 1 geschrieben

---
+++ ixts.Ausbildung.Newton.Test/ApproximationTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace ixts.Ausbildung.Newton.Test
+{
+    [TestFixture]
+    public class ApproximationTests
+    {
+
+        [TestCase(25.0,5.0)]
+        [TestCase(144.0,12.0)]
+        public void CalculateTest(double number,double expected)
+        {
+            var actual = Approximation.Calculate(number);
+
+            Assert.AreEqual(expected, actual, 0.000000001);
+        }
+    }
+}
---
+++ ixts.Ausbildung.Newton/Approximation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ixts.Ausbildung.Newton
+{
+    public class Approximation
+    {
+
+        public static double Calculate(double number)
+        {
+            var random = new Random();
+            var q = (double)random.Next(1, (int) number/2);
+
+
+            while (Math.Abs(number - q*q) >= 0.000000001)
+            {
+                    q = (q + (number/q))/2;
+            }
+
+            return q;
+        }
+    }
+}
