8089e6ceb3d43c4d6c3566c58cd29c6e4bbd930c
unknown <mkaestl@XDEV007.ixts.local>
2014-11-06 10:08:37 +0100

    Aufgabe Binomialkoeffizienten geschrieben

---
+++ ixts.Ausbildung.Binomialkoeffizienten.Test/PascalTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace ixts.Ausbildung.Binomialkoeffizienten.Test
+{
+    [TestFixture]
+    public class PascalTests
+    {
+        [TestCase(3, 2, 5, 3)]
+        [TestCase(2, 3, 5, 0)]
+        public void CalculateTest(int n,int k,int m,int expected)
+        {
+            var pascal = new Pascal(m);
+            var actual = pascal.Calculate(n, k);
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [ExpectedException]
+        [TestCase(17,16,5)]
+        public void OverflowTest(int n,int k,int m)
+        {
+            var pascal = new Pascal(m);
+            pascal.Calculate(n, k);
+        }
+    }
+}
---
+++ ixts.Ausbildung.Binomialkoeffizienten/PascalOpt.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ixts.Ausbildung.Binomialkoeffizienten
+{
+    public class PascalOpt
+    {
+        private readonly int[][] triangle;
+
+        public PascalOpt(int m)
+        {
+            triangle = new int[m + 1][];
+            for (var n = 0; n <= m; n++)
+            {
+                triangle[n] = new int[n/2 + 1];
+                triangle[n][0] = 1;
+
+                for (var k = 0; k < (n + 1)/2; k++)
+                {
+                    triangle[n][k] = triangle[n - 1][k - 1] + triangle[n- 1][k];
+                    if (n > 0 && n%2 == 0)
+                    {
+                        triangle[n][n/2] = 2*triangle[n - 1][n/2 - 1];
+                    }
+                }
+            }
+        }
+
+        public int Calculate(int n,int k)
+        {
+            if (k > n/2)
+            {
+                k = n - k;
+            }
+
+            return k < 0 ? 0 : triangle[n][k];
+        }
+
+        public override String ToString()
+        {
+            var m = triangle.Length - 1;
+            var s = "";
+            for (var n = 0; n <= m; n++)
+            {
+                for (var l = 0; l < m - n; l++)
+                {
+                    s += " ";
+                }
+                for (var k = 0; k <= n; k++)
+                {
+                    s += String.Format("{0}", Calculate(n, k));
+                }
+                s += Environment.NewLine;
+            }
+            return s;
+        }
+
+    }
+}
