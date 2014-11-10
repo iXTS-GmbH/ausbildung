5227b23786414315b381a2d835c962ab0c633ae2
unknown <mkaestl@XDEV007.ixts.local>
2014-11-10 12:16:41 +0100

    Aufgabe Mobiles geschrieben

---
+++ ixts.Ausbildung.Mobiles/GlitterStar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ixts.Ausbildung.Mobiles
+{
+    public class GlitterStar:Star
+    {
+        public GlitterStar(int weight) : base(weight)
+        {
+        }
+
+        public void Decorate()
+        {
+            weight = weight + 1;
+        }
+
+        public override String ToString()
+        {
+            return string.Format("Glitterstar[{0}]", weight);
+        }
+    }
+}
---
+++ ixts.Ausbildung.Mobiles/Mobile.cs
@@ -0,0 +1,26 @@
+﻿namespace ixts.Ausbildung.Mobiles
+{
+    public class Mobile:IMobile
+    {
+        private readonly Wire mobile;
+
+        public Mobile(Wire mobile)
+        {
+            this.mobile = mobile;
+        }
+
+        public double Weight()
+        {
+            return mobile.Weight();
+        }
+
+        public void Balance()
+        {
+        }
+
+        public override string ToString()
+        {
+           return mobile.ToString();
+        }
+    }
+}
