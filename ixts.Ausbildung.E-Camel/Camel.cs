97a410904dbdde193f9060a75f119380ebd2b922
unknown <mkaestl@XDEV007.ixts.local>
2014-11-10 11:17:25 +0100

    Aufgabe E-Camel geschrieben

---
+++ ixts.Ausbildung.E-Camel/Camel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ixts.Ausbildung.E_Camel
+{
+    public class Camel
+    {
+        private readonly int maxTravelSpeed;
+        private int pace;
+        private Camel next;
+
+        public Camel(int mp)
+        {
+            maxTravelSpeed = mp;
+        }
+
+        public int MaxPace()
+        {
+            return maxTravelSpeed;
+        }
+
+        public int Pace()
+        {
+            if (maxTravelSpeed - pace <= 0)
+            {
+                return 0;
+            }
+
+            return maxTravelSpeed - pace;
+        }
+
+        public void SetLoad(int l)
+        {
+            pace = l;
+        }
+
+        public int GetLoad()
+        {
+            return pace;
+        }
+
+        public void SetNext(Camel c)
+        {
+            if (this == c)
+            {
+                throw new Exception("Camel can't be self-follower");
+            }
+
+
+            if (c != null)
+            {
+                c.SetNext(next);
+            }
+            
+            next = c;
+        }
+
+        public Camel GetNext()
+        {
+            return next;
+        }
+
+    }
+}
---
+++ ixts.Ausbildung.E-Camel/Caravan.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ixts.Ausbildung.E_Camel
+{
+    public class Caravan
+    {
+        private readonly List<Camel> caravan; 
+
+        public Caravan()
+        {
+            caravan = new List<Camel>();
+        }
+
+        public int Pace()
+        {
+            if (caravan.Count == 0)
+            {
+                throw new Exception("Empty Caravan");
+            }
+
+            var pace = caravan[0].Pace();
+
+            foreach (var camel in caravan)
+            {
+                if (camel.Pace() < pace)
+                {
+                    pace = camel.Pace();
+                }
+            }
+            return pace;
+        }
+
+        public void AddCamel(Camel c)
+        {
+            if (c == null)
+            {
+                throw new Exception("Invalid Argument");
+            }
+
+            foreach (var camel in caravan)
+            {
+                if (c == camel)
+                {
+                    throw new Exception("Camel already in Caravan");
+                }
+            }
+
+            if (caravan.Count > 0)
+            {
+                caravan[caravan.Count - 1].SetNext(c);
+            }
+
+            caravan.Add(c);
+        }
+
+        public void RemoveCamel(Camel c)
+        {
+            if (caravan.Count > 0)
+            {
+                caravan.Remove(c);
+            }
+            else
+            {
+                throw new Exception("Empty Caravan");
+            }
+            
+        }
+
+        public void Unload()
+        {
+            if (caravan.Count == 0)
+            {
+                throw new Exception("Empty Caravan");
+            }
+
+            foreach (var camel in caravan)
+            {
+                camel.SetLoad(0);
+            }
+        }
+
+        public void AddLoad(int l)
+        {
+            if (l < 0)
+            {
+                throw new Exception("Invalid Argument");
+            }
+
+            if (caravan.Count == 0)
+            {
+                throw new Exception("Empty Caravan");
+            }
+
+            while (l != 0)
+            {
+                var maxSpeed = 0;
+                foreach (var camel in caravan)
+                {
+                    if (camel.Pace() > maxSpeed)
+                    {
+                        maxSpeed = camel.Pace();
+                    }
+                }
+
+                foreach (var camel in caravan)
+                {
+                    if (camel.Pace() == maxSpeed && l != 0)
+                    {
+                        camel.SetLoad(camel.GetLoad() + 1);
+                        l = l - 1;
+                    }
+                }
+            }
+        }
+    }
+}
