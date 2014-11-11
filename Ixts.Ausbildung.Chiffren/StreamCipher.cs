37737180a4b18e64eb271db3ae97222c474246e3
unknown <mkaestl@XDEV007.ixts.local>
2014-11-11 08:52:30 +0100

    Aufgabe Chiffren geschrieben

---
+++ Ixts.Ausbildung.Chiffren/StreamCipher.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Ixts.Ausbildung.Chiffren
+{
+    public class StreamCipher:ICipher
+    {
+        private Random keyGenerator;
+
+        public string Encrypt(string plainText, int key)
+        {
+            keyGenerator = new Random(key);
+
+            var bytes = new byte[plainText.Length];
+
+            for (var i = 0; i < plainText.Length; i++)
+            {
+                bytes[i] = Convert.ToByte(plainText[i]);
+            }
+
+            var cipher = new XorSubstitution();
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                var runkey = keyGenerator.Next();
+                bytes[i] = cipher.Encrypt(bytes[i], runkey);
+            }
+            var cryptText = "";
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                cryptText = Convert.ToChar(bytes[i]) + cryptText;
+            }
+            return cryptText;
+        }
+
+        public string Decrypt(string cryptText, int key)
+        {
+            keyGenerator = new Random(key);
+
+            var bytes = new byte[cryptText.Length];
+
+            for (var i = 0; i < cryptText.Length; i++)
+            {
+                bytes[i] = Convert.ToByte(cryptText[i]);
+            }
+
+            var cipher = new XorSubstitution();
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                var runkey = keyGenerator.Next();
+                bytes[i] = cipher.Decrypt(bytes[i], runkey);
+            }
+            var plainText = "";
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                plainText = Convert.ToChar(bytes[i]) + plainText;
+            }
+            return plainText;
+        }
+    }
+}
---
+++ Ixts.Ausbildung.Chiffren/XorSubstitution.cs
@@ -0,0 +1,15 @@
+﻿namespace Ixts.Ausbildung.Chiffren
+{
+    public class XorSubstitution:Sustitution
+    {
+        public override byte Encrypt(byte c, int key)
+        {
+            return (byte)(c^key);
+        }
+
+        public override byte Decrypt(byte c, int key)
+        {
+            return (byte)(c^key);
+        }
+    }
+}
