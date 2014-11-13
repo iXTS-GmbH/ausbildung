8f5b53089cb5d235c2cb511d20f9902b5eb34dc5
unknown <mkaestl@XDEV007.ixts.local>
2014-11-13 12:22:12 +0100

    Aufgabe I/O-Filter geschrieben

---
+++ ixts.Ausbildung.I-O-Filter/PositionReader.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace ixts.Ausbildung.I_O_Filter
+{
+    public class PositionReader:TextReader
+    {
+        private const int TAB = 8;
+        private int line = 1;
+        private int column = 0;
+        private readonly Boolean unixSystem = Environment.NewLine == "\r\n";
+
+        public override int Read()
+        {
+            return TrackPosition(base.Read());
+        }
+
+        public override int Read(char[] buffer, int index, int count)
+        {
+            int have = base.Read(buffer, index, count);
+            for (int i = index; i < index + have; i++)
+            {
+                TrackPosition(buffer[i]);
+            }
+            return have;
+        }
+
+        private int TrackPosition(int chr)
+        {
+            if (chr < 0)
+            {
+                
+            }else if (chr == '\n')
+            {
+                line++;
+                if (unixSystem)
+                {
+                    column = 0;
+                }
+            }else if (chr == '\t')
+            {
+                column = (column/TAB + 1)*TAB;
+            }else if (chr == '\r')
+            {
+                column = 0;
+            }
+            else
+            {
+                column++;
+            }
+            return chr;
+        }
+
+        public int Line()
+        {
+            return line;
+        }
+
+        public int Column()
+        {
+            return column;
+        }
+    }
+}
---
+++ ixts.Ausbildung.I-O-Filter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("ixts.Ausbildung.I-O-Filter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ixts.Ausbildung.I-O-Filter")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("5fe3096d-4ea2-4724-b004-5dda36c26003")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
