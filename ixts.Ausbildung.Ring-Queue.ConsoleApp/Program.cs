f5b336aeeb9effc2509f0b34c48bf7b39618fcd4
unknown <mkaestl@XDEV007.ixts.local>
2014-11-20 12:41:41 +0100

    Aufgabe RingQueue geschrieben

---
+++ ixts.Ausbildung.Ring-Queue.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ixts.Ausbildung.Ring_Queue.ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var ring = new RingQueue<int>(1, 1, 1);
+
+            for (var i = 0; i < 16; i++)
+            {
+                var a = ring.Get(2);
+                var b = ring.Get(1);
+                var c = ring.Get(0);
+
+                ring.Push(a+b+c);
+            }
+
+            Console.WriteLine(ring.Get(0));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ixts.Ausbildung.Ring-Queue.Test/RingQueueTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace ixts.Ausbildung.Ring_Queue.Test
+{
+    [TestFixture]
+    public class RingQueueTests
+    {
+
+
+        [TestCase(0,5)]
+        public void SizeTest(int expected,int capacity)
+        {
+            var ring = new RingQueue<int>(capacity);
+
+            var actual = ring.Size();
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestCase(3,1,2,3,4,5,6)]
+        public void PushGetTest(int capacity,params int[] values)
+        {
+            var ring = new RingQueue<int>(capacity);
+
+            foreach (var value in values)
+            {
+                ring.Push(value);
+            }
+
+            Assert.AreEqual(values[5], ring.Get(0));
+            Assert.AreEqual(values[4], ring.Get(1));
+            Assert.AreEqual(values[3], ring.Get(2));
+
+            for (var i = values.Length -1; i > values.Length - 4; i--)
+            {
+                Assert.AreEqual(values[i],ring.Get(values.Length -1 - i));
+            }
+        }
+
+        [ExpectedException]
+        [TestCase(1,2,3,4,5,6)]
+        public void ClearTest(params int[] values)
+        {
+            var ring = new RingQueue<int>(values);
+
+            ring.Clear();
+
+            ring.Get(0);
+
+        }
+    }
+}
