2d1b672e7ee09f6e052d261137b9da76fd1859d2
unknown <mkaestl@XDEV007.ixts.local>
2014-11-24 08:36:37 +0100

    Aufgabe Algebraische Strukturen geschrieben

---
+++ ixts.Ausbildung.Algebraische_Strukturen.Test/ZPTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ixts.Ausbildung.Algebraische_Strukturen.Test
+{
+    [TestFixture]
+    public class ZPTests
+    {
+
+        [TestCase(1, 1, 0)]
+        public void AddTest(int expectedvalue, int value1, int value2)
+        {
+            var a = new Z2(value1);
+            var b = new Z2(value2);
+            var expected = new Z2(expectedvalue);
+            var actual = a.Add(b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1, 1, 0)]
+        public void SubTest(int expectedvalue, int value1, int value2)
+        {
+            var a = new Z2(value1);
+            var b = new Z2(value2);
+            var expected = new Z2(expectedvalue);
+            var actual = a.Sub(b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1, 1, 1)]
+        public void MultTest(int expectedvalue, int value1, int value2)
+        {
+            var a = new Z2(value1);
+            var b = new Z2(value2);
+            var expected = new Z2(expectedvalue);
+            var actual = a.Mult(b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1, 1, 1)]
+        public void DivTest(int expectedvalue, int value1, int value2)
+        {
+            var a = new Z2(value1);
+            var b = new Z2(value2);
+            var expected = new Z2(expectedvalue);
+            var actual = a.Mult(b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1)]
+        public void GetHashCodeTest(int value)
+        {
+            var a = new Z2(value);
+
+            Assert.AreEqual(value, a.GetHashCode());
+        }
+
+        [TestCase]
+        public void IsOneTest()
+        {
+            var a = new Z2(1);
+
+            Assert.IsTrue(a.IsOne());
+        }
+
+        [TestCase]
+        public void IsZeroTest()
+        {
+            var a = new Z2(0);
+
+            Assert.IsTrue(a.IsZero());
+        }
+
+        [TestCase("1", 1)]
+        public void ToStringTest(String expected, int value)
+        {
+            var a = new Z2(value);
+            var actual = a.ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ ixts.Ausbildung.Algebraische_Strukturen/ZP.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using ixts.Ausbildung.Primzahlen;
+using ixts.Ausbildung.Potenzieren;
+
+namespace ixts.Ausbildung.Algebraische_Strukturen
+{
+    public class ZP:IField<ZP>
+    {
+        private readonly int prime;
+        private readonly int value;
+
+        public ZP(int p,int i)
+        {
+            if (!PrimeTest.IsPrime(p))
+            {
+                throw new Exception(string.Format("not a Prime {0}",p));
+            }
+
+            prime = p;
+            value = (i%p+ p)%p;
+        }
+
+        public ZP Div(ZP x)
+        {
+            if (IsZero())
+            {
+                throw new Exception("Division by Zero");
+            }
+            return Mult(x.Inverse());
+        }
+
+        public ZP Inverse()
+        {
+            if (IsZero())
+            {
+                throw new Exception("Division by Zero");
+            }
+            return new ZP(prime, Potenz.Pow(value,prime - 2,prime));
+        }
+
+        public ZP Mult(ZP x)
+        {
+            if (prime != x.prime)
+            {
+                throw new Exception("Different Field");
+            }
+            return new ZP(prime,value*x.value);
+        }
+
+        public bool IsOne()
+        {
+            return value == 1;
+        }
+
+        public ZP Add(ZP x)
+        {
+            if (prime != x.prime)
+            {
+                throw new Exception("Different Field");
+            }
+            return new ZP(prime,value+x.value);
+        }
+
+        public ZP Sub(ZP x)
+        {
+            if (prime != x.prime)
+            {
+                throw new Exception("Different Field");
+            }
+            return new ZP(prime, value - x.value);
+        }
+
+        public bool IsZero()
+        {
+            return value == 0;
+        }
+
+        public override bool Equals(object x)
+        {
+            if (x == null)
+            {
+                return false;
+            }
+            if (GetType() != x.GetType())
+            {
+                return false;
+            }
+
+            var other = (ZP)x;
+
+            return other.value == value && other.prime == prime;
+        }
+
+        public override int GetHashCode()
+        {
+            var hash = 17;
+            hash = 53*hash + prime;
+            hash = 53*hash + value;
+            return hash;
+        }
+    }
+}
