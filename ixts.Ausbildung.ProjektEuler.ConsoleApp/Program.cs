66f84eb1cb0c512a5c2d7750310617d891982f7b
unknown <mkaestl@XDEV007.ixts.local>
2014-11-28 11:23:18 +0100

    Projekt Euler Problem 1 Solved

---
+++ ixts.Ausbildung.ProjektEuler.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ixts.Ausbildung.ProjektEuler.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Multiplesof3And5.CalculateSum());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ixts.Ausbildung.ProjektEuler/Multiplesof3And5.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ixts.Ausbildung.ProjektEuler
+{
+    public class Multiplesof3And5
+    {
+        public static int CalculateSum()
+        {
+            var result = 0;
+
+            for (var i = 1; i < 1000; i++)
+            {
+                if (i%3 == 0 || i%5 == 0)
+                {
+                    result += i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
